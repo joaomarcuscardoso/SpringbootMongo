b534d3de9f7cf8823c9ce3ce300220737ea21b5d
joaomarcuscardoso <rfcjoujou@gmail.com>
2021-08-02 23:54:30 -0300

    Obtendo post por id

--- src/main/java/com/nelioalves/workshopmongo/domain/Post.java
+++ src/main/java/com/nelioalves/workshopmongo/domain/Post.java
@@ -24,6 +24,7 @@ public class Post implements Serializable {
     }
 
     public Post(String id, Date date, String title, String body, AuthorDTO author) {
+        super();
         this.id = id;
         this.date = date;
         this.title = title;
